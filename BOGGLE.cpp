b208fe94655eb1a10852e558d232ac53f75b3667
ChoH96 <67941898+ChoH96@users.noreply.github.com>
2020-07-21 20:26:16 +0900

    optimized in new way

--- BOGGLE.cpp
+++ BOGGLE.cpp
@@ -2,29 +2,37 @@
 //
 #include <iostream>
 
+//all 8 directions
 const int dr[8] = { -1,-1, 0, 1, 1, 1, 0,-1 };
 const int dc[8] = {  0, 1, 1, 1, 0,-1,-1,-1 };
 
-
+// check index r,c is within the limit
 int isValid(int r, int c) {
 	if (0 <= r && r < 5 && 0 <= c && c < 5) return 1;
 	return 0;
 }
 
-int hasWord(int r, int c, char* word, int idx, char arr[][6]) {
-	int i, j;
-	int new_r, new_c;
+// check using movable points(map)
+int hasWord(char* word, int idx, char arr[][6], int map[][5]) {
+	int i, j, k;
+	int cnt = 0;
+	int newMap[5][5] = { 0 };// next movable points
 	if (word[idx] == '\0') return 1;
-	for (i = 0; i < 8; i++) {
-		new_r = r + dr[i];
-		new_c = c + dc[i];
-		if (isValid(new_r, new_c) && arr[new_r][new_c] == word[idx]) {
-			if (hasWord(new_r, new_c, word, idx + 1, arr)) {
-				return 1;
+	
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5; j++) {
+			if (map[i][j] && word[idx] == arr[i][j]) {
+				for (k = 0; k < 8; k++) {
+					if (isValid(i + dr[k], j + dc[k])) {
+						newMap[i + dr[k]][j + dc[k]] = 1;
+						cnt++;
+					}
+				}
 			}
 		}
 	}
-	return 0;
+	if (cnt) return hasWord(word, idx + 1, arr, newMap);
+	else return 0;
 }
 
 
@@ -33,6 +41,12 @@ int main()
 	int i, j;
 	int testCase, numWord;
 	char arr[5][6], word[11];
+	int map[5][5];
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 5; j++) {
+			map[i][j] = 1;
+		}
+	}
 
 	scanf("%d", &testCase);
 	while (testCase) {
@@ -44,16 +58,11 @@ int main()
 		scanf("%d", &numWord);
 		while(numWord) {
 			scanf("%s", word);
-			for (i = 0; i < 5; i++) {
-				for (j = 0; j < 5; j++) {
-					if (arr[i][j] == word[0] && hasWord(i,j,word,1,arr)) {
-						printf("%s YES\n", word);
-						i = 6;
-						j = 6;
-					}
-				}
+			
+			if (hasWord(word, 0, arr, map)) {
+				printf("%s YES\n", word);
 			}
-			if (i == 5 && j == 5) {
+			else {
 				printf("%s NO\n", word);
 			}
 
