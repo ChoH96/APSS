57af235a5b009c4322e061e30b0808eb94631b27
ChoH96 <67941898+ChoH96@users.noreply.github.com>
2020-08-05 22:01:20 +0900

    LIS improved
    use binary search to improve more

---
+++ LIS2.cpp
@@ -0,0 +1,56 @@
+﻿//https://algospot.com/judge/problem/read/LIS
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//ios_base::sync_with_stdio(false)
+//try to use '\n' than endl
+
+
+//return Length of increasing subsequence 
+int LIS(vector<int> arr) {
+	int i, j;
+	int answer = 0;
+	vector<int> lastValue(arr.size() + 1, -1); // use index 0 ~ N-1, N is not used
+
+	//find LIS that include and start from arr[i]  
+	lastValue[0] = arr[0];
+	for (i = 1; i < arr.size(); i++) {
+		for (j = 0; lastValue[j] != -1 && lastValue[j] < arr[i]; j++);// +more optimization can be done using binary search
+		lastValue[j] = arr[i];
+	}
+
+	//find max
+	// +more optimization can be done using binary search
+	for (i = 0; lastValue[i] != -1; i++);
+	answer = i;
+
+	return answer;
+}
+
+
+
+int main()
+{
+	int i, j, tmp;
+	int testCase;
+	int size;
+
+	cin >> testCase;
+	while (testCase) {
+		//inputs
+		cin >> size;
+
+		vector<int> arr(size);
+		for (i = 0; i < size; i++) {
+			cin >> arr[i];
+		}
+
+		cout << LIS(arr) << '\n';
+
+		testCase--;
+	}
+	return 0;
+}
