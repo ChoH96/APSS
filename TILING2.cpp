38f4e3ced320e93234002bf639321f8a2dbec6f9
ChoH96 <67941898+ChoH96@users.noreply.github.com>
2020-08-17 18:34:57 +0900

    simple math

---
+++ TILING2.cpp
@@ -0,0 +1,41 @@
+//https://algospot.com/judge/problem/read/
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//ios_base::sync_with_stdio(false)
+//try to use '\n' than endl
+
+//simple math with memorization
+int tiling(vector<int>& mem, int size) {
+	int ret;
+	if (mem[size] != -1) return mem[size];
+	
+	ret = (tiling(mem, size - 1) + tiling(mem, size - 2)) % 1000000007;
+	mem[size] = ret;
+	return ret;
+}
+
+
+int main()
+{
+	int i, j, tmp;
+	int testCase;
+	int size;
+
+	vector<int> mem(101, -1);
+	mem[0] = 1;
+	mem[1] = 1;
+	cin >> testCase;
+	while (testCase) {
+		//inputs
+		cin >> size;
+
+		cout << tiling(mem, size) << '\n';
+
+		testCase--;
+	}
+	return 0;
+}
